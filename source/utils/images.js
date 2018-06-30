f9bdba2aaca44d0052f8595a3ccdf0a39ab257a5
Bogdan Volotovskyy <bvol@carbook.ua>
2018-07-01 01:07:44 +0300

    feature(core): style funel & daterange filter

--- source/utils/images.js
+++ source/utils/images.js
@@ -4,6 +4,13 @@ import exception400 from 'theme/images/exceptions/400.svg';
 import exception404 from 'theme/images/exceptions/404.svg';
 import exception500 from 'theme/images/exceptions/500.svg';
 
+import funelTopLeft from 'theme/images/funel/funel_top-left.svg';
+import funelRight from 'theme/images/funel/funel_right.svg';
+import funelTopRight from 'theme/images/funel/funel_top-right.svg';
+import funelBottomLeft from 'theme/images/funel/funel_bottom-left.svg';
+import funelLeft from 'theme/images/funel/funel_left.svg';
+import funelBottomRight from 'theme/images/funel/funel_bottom-right.svg';
+
 export default {
     // Layout
     carbookLogo,
@@ -11,4 +18,11 @@ export default {
     exception400,
     exception404,
     exception500,
+    // Funel
+    funelTopLeft,
+    funelLeft,
+    funelRight,
+    funelTopRight,
+    funelBottomLeft,
+    funelBottomRight,
 };
