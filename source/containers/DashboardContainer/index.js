0a28fd6d1706aebc250cb612a8ae7d86dd6f94d9
Bogdan Volotovskyy <bvol@carbook.ua>
2018-07-17 16:53:47 +0300

    chore(ui): static grid concept v1

--- source/containers/DashboardContainer/index.js
+++ source/containers/DashboardContainer/index.js
@@ -87,68 +87,57 @@ export default class DashboardContainer extends Component {
                         <div className={ Styles.gridEmptyCell } />
                         <div className={ Styles.gridEmptyCell } />
                         <div className={ Styles.gridEmptyCell } />
-                        <div className={ Styles.gridEmptyCell } />
+                        { /* <div className={ Styles.gridEmptyCell } /> */ }
                         { /* </div> */ }
                     </div>
                     <div className={ Styles.gridColumn }>
-                        <div className={ Styles.gridHead }>2</div>
-                        { time.map(() => (
+                        <div
+                            className={ Styles.gridHead }
+                            // style={ { gridRow: 'span 2' } }
+                        >
+                            2
+                        </div>
+                        <div className='content'>
+                            <div className='order1'>order 11111</div>
+                            { /* <div className='order1'>order 11111</div> */ }
+                            <div className='order2'>order 22222</div>
+                            <div className={ Styles.gridEmptyCell } />
+                            <div className={ Styles.gridEmptyCell } />
+                        </div>
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className='content2'>
+                            <div className='order4'>order 11111</div>
+                        </div>
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        <div className={ Styles.gridEmptyCell } />
+                        { /* <div className={ Styles.gridEmptyCell } /> */ }
+                        { /* <div className={ Styles.gridEmptyCell } /> */ }
+                        { /* <div className={ Styles.gridEmptyCell } /> */ }
+
+                        { /* { time.map(() => (
                             <div key={ v4() } className={ Styles.gridContent }>
                                 <div className={ Styles.gridEmptyCell } />
                                 <div className={ Styles.gridEmptyCell } />
                             </div>
-                        )) }
+                        )) } */ }
                     </div>
-                    <div className={ Styles.gridColumn }>
-                        <div className={ Styles.gridHead }>3</div>
-                        { /* <div className={ Styles.gridContent }> */ }
-                        <div className={ Styles.gridContent }>
-                            <div className='order1'>order 11111</div>
-                            <div className='order2'>order 22222</div>
-                            { /* <div className='order1'>order 123</div> */ }
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className='order1'>order 11111</div>
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className={ Styles.gridEmptyCell } />
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className='order3'>order 33333</div>
-                            { /* <div className={ Styles.gridEmptyCell } /> */ }
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className={ Styles.gridEmptyCell } />
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className={ Styles.gridEmptyCell } />
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className='order4'>order 44444</div>
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className={ Styles.gridEmptyCell } />
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className={ Styles.gridEmptyCell } />
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className={ Styles.gridEmptyCell } />
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        <div className={ Styles.gridContent }>
-                            <div className={ Styles.gridEmptyCell } />
-                            <div className={ Styles.gridEmptyCell } />
-                        </div>
-                        { /* </div> */ }
-                    </div>
+                    <div className={ Styles.gridColumn } />
                     <div className={ Styles.gridColumn }>
                         <div className={ Styles.gridHead }>4</div>
                         <div className={ Styles.gridContent }>
---
+++ source/containers/DashboardContainer/stash.js
@@ -0,0 +1,48 @@
+<div className={ Styles.gridHead }>3</div>
+{ /* <div className={ Styles.gridContent }> */ }
+<div className={ Styles.gridContent }>
+    <div className='order1'>order 11111</div>
+    <div className='order2'>order 22222</div>
+    { /* <div className='order1'>order 123</div> */ }
+</div>
+<div className={ Styles.gridContent }>
+    <div className='order1'>order 11111</div>
+    <div className={ Styles.gridEmptyCell } />
+</div>
+<div className={ Styles.gridContent }>
+    <div className={ Styles.gridEmptyCell } />
+    <div className={ Styles.gridEmptyCell } />
+</div>
+<div className={ Styles.gridContent }>
+    <div className='order3'>order 33333</div>
+    { /* <div className={ Styles.gridEmptyCell } /> */ }
+</div>
+<div className={ Styles.gridContent }>
+    <div className={ Styles.gridEmptyCell } />
+    <div className={ Styles.gridEmptyCell } />
+</div>
+<div className={ Styles.gridContent }>
+    <div className={ Styles.gridEmptyCell } />
+    <div className={ Styles.gridEmptyCell } />
+</div>
+<div className={ Styles.gridContent }>
+    <div className='order4'>order 44444</div>
+    <div className={ Styles.gridEmptyCell } />
+</div>
+<div className={ Styles.gridContent }>
+    <div className={ Styles.gridEmptyCell } />
+    <div className={ Styles.gridEmptyCell } />
+</div>
+<div className={ Styles.gridContent }>
+    <div className={ Styles.gridEmptyCell } />
+    <div className={ Styles.gridEmptyCell } />
+</div>
+<div className={ Styles.gridContent }>
+    <div className={ Styles.gridEmptyCell } />
+    <div className={ Styles.gridEmptyCell } />
+</div>
+<div className={ Styles.gridContent }>
+    <div className={ Styles.gridEmptyCell } />
+    <div className={ Styles.gridEmptyCell } />
+</div>
+{ /* </div> */ }
