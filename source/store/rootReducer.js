a3fa593395b776eae7807b93692b70fe8cabffd5
Bogdan Volotovskyy <bvol@carbook.ua>
2018-09-10 17:13:32 +0300

    improve(core): global reducers

--- source/store/rootReducer.js
+++ source/store/rootReducer.js
@@ -4,6 +4,7 @@ import { routerReducer as router } from 'react-router-redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 import { LOCATION_CHANGE } from 'react-router-redux';
+import _ from 'lodash';
 
 // proj
 import intl from 'core/intl/reducer';
@@ -43,11 +44,13 @@ export const persistConfig = {
     whitelist: [ 'auth' ],
 };
 
-const persistedReducer = combineReducers({
+const persistedState = {
     [ authModule ]: authReducer,
-});
+    intl,
+    router,
+};
 
-const appReducer = combineReducers({
+const appState = {
     forms,
     [ ordersModule ]:          ordersReducer,
     [ orderModule ]:           orderReducer,
@@ -64,30 +67,47 @@ const appReducer = combineReducers({
     [ clientsModule ]:         clientsReducer,
     [ clientModule ]:          clientReducer,
     [ clientRequisiteModule ]: clientRequisiteReducer,
-    intl,
-    router,
-});
-
-const reducer = (state, action) => {
-    let initialState = state;
-    if (action.type === LOCATION_CHANGE) {
-        console.log('→LOCATION_CHANGE 111', state);
-
-        initialState = void 0;
-    }
-    // console.log('→LOCATION_CHANGE 222', state);
-    console.log('aaa', {
-        ...appReducer(initialState, action),
-        ...persistedReducer(state, action),
-    });
-    console.log('→ state', state);
-    console.log('→ initialState', initialState);
-
-    return {
-        ...appReducer(initialState, action),
-        ...persistedReducer(state, action),
-    };
+    // [ authModule ]:            authReducer,
+    // intl,
+    // router,
 };
+
+const persistedReducer = combineReducers(persistedState);
+
+const appReducer = combineReducers(appState);
+
+const globalReducer = combineReducers({ ...persistedState, ...appState });
+console.log('→ 111111');
+// const reducer = (state, action) => {
+//     let initialState = state;
+//     if (action.type === LOCATION_CHANGE) {
+//         console.log('→LOCATION_CHANGE 111', state);
+//         let persistedState = _.pick(initialState, 'auth');
+//         initialState = void 0;
+//     }
+//     // console.log('→LOCATION_CHANGE 222', state);
+//     console.log('→ state', state);
+//
+//     return appReducer(state, action);
+// };
+const reducer = (state, action) => {
+    console.log('→ reducer action', action);
+    let initialState = state;
+    const auth = _.pick(initialState, 'auth');
+    if (action.type === LOCATION_CHANGE) {
+        console.log('→1111111LOCATION_CHANGE __ыефеу', state);
+
+        initialState = {
+            ...auth,
+        };
+        console.log('→1111111LOCATION_CHANGE _init', initialState);
+    }
+    console.log('→LOCATION_CHANGE __ыефеу', state);
+    console.log('→LOCATION_CHANGE _init', initialState);
+
+    return globalReducer(initialState, action);
+};
+// console.log('→ 222222', reducer());
 
 // const reducer = (state, action) =>
 //     appReducer(
@@ -99,8 +119,33 @@ const reducer = (state, action) => {
 //             : state,
 //         action,
 //     );
-console.log('→ reducer', reducer);
 
-const rootReducer = persistReducer(persistConfig, reducer);
+const rootReducer = persistReducer(persistConfig, (state, action) => {
+    console.log('→ persistReducer state', state);
+    console.log('→ persistReducer action', action);
 
+    return reducer(state, action);
+});
+console.log('→ 33333');
 export default rootReducer;
+
+// const reducer = (state, action) => {
+//     let initialState = state;
+//     if (action.type === LOCATION_CHANGE) {
+//         console.log('→LOCATION_CHANGE 111', state);
+//
+//         initialState = void 0;
+//     }
+//     // console.log('→LOCATION_CHANGE 222', state);
+//     console.log('aaa', {
+//         ...persistedReducer(state, action),
+//         ...appReducer(initialState, action),
+//     });
+//     console.log('→ state', state);
+//     console.log('→ initialState', initialState);
+//
+//     return {
+//         ...persistedReducer(state, action),
+//         ...appReducer(initialState, action),
+//     };
+// };
