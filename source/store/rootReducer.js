d84966b2f7ea6a53a8c60479ee3108779454aa51
Bogdan Volotovskyy <bvol@carbook.ua>
2018-09-10 17:24:56 +0300

    feature(core): reset store on location change

--- source/store/rootReducer.js
+++ source/store/rootReducer.js
@@ -4,7 +4,6 @@ import { routerReducer as router } from 'react-router-redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 import { LOCATION_CHANGE } from 'react-router-redux';
-import _ from 'lodash';
 
 // proj
 import intl from 'core/intl/reducer';
@@ -72,80 +71,27 @@ const appState = {
     // router,
 };
 
-const persistedReducer = combineReducers(persistedState);
+const appReducer = combineReducers({ ...persistedState, ...appState });
 
-const appReducer = combineReducers(appState);
-
-const globalReducer = combineReducers({ ...persistedState, ...appState });
-console.log('→ 111111');
-// const reducer = (state, action) => {
-//     let initialState = state;
-//     if (action.type === LOCATION_CHANGE) {
-//         console.log('→LOCATION_CHANGE 111', state);
-//         let persistedState = _.pick(initialState, 'auth');
-//         initialState = void 0;
-//     }
-//     // console.log('→LOCATION_CHANGE 222', state);
-//     console.log('→ state', state);
-//
-//     return appReducer(state, action);
-// };
 const reducer = (state, action) => {
-    console.log('→ reducer action', action);
-    let initialState = state;
-    const auth = _.pick(initialState, 'auth');
+    let resetedState = state;
+
     if (action.type === LOCATION_CHANGE) {
-        console.log('→1111111LOCATION_CHANGE __ыефеу', state);
+        resetedState = Object.keys(persistedState).reduce(
+            (resetedState, moduleName) => {
+                resetedState[ moduleName ] = state[ moduleName ];
 
-        initialState = {
-            ...auth,
-        };
-        console.log('→1111111LOCATION_CHANGE _init', initialState);
+                return resetedState;
+            },
+            {},
+        );
     }
-    console.log('→LOCATION_CHANGE __ыефеу', state);
-    console.log('→LOCATION_CHANGE _init', initialState);
 
-    return globalReducer(initialState, action);
+    return appReducer(resetedState, action);
 };
-// console.log('→ 222222', reducer());
-
-// const reducer = (state, action) =>
-//     appReducer(
-//         action.type === LOCATION_CHANGE
-//             ? {
-//                 ...appReducer({}, {}),
-//                 router: state && state.router || {},
-//             }
-//             : state,
-//         action,
-//     );
 
 const rootReducer = persistReducer(persistConfig, (state, action) => {
-    console.log('→ persistReducer state', state);
-    console.log('→ persistReducer action', action);
-
     return reducer(state, action);
 });
-console.log('→ 33333');
-export default rootReducer;
 
-// const reducer = (state, action) => {
-//     let initialState = state;
-//     if (action.type === LOCATION_CHANGE) {
-//         console.log('→LOCATION_CHANGE 111', state);
-//
-//         initialState = void 0;
-//     }
-//     // console.log('→LOCATION_CHANGE 222', state);
-//     console.log('aaa', {
-//         ...persistedReducer(state, action),
-//         ...appReducer(initialState, action),
-//     });
-//     console.log('→ state', state);
-//     console.log('→ initialState', initialState);
-//
-//     return {
-//         ...persistedReducer(state, action),
-//         ...appReducer(initialState, action),
-//     };
-// };
+export default rootReducer;
