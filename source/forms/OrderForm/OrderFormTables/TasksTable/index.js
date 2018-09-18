d5ab9df49a5f1c5950ee6e1f89cad64ef3b2c799
Oleh Yanivskyy <yan@carbook.ua>
2018-09-18 13:07:42 +0300

    fix(ui): pro-883 duplicates in tasks

--- source/forms/OrderForm/OrderFormTables/TasksTable/index.js
+++ source/forms/OrderForm/OrderFormTables/TasksTable/index.js
@@ -4,6 +4,7 @@ import { FormattedMessage } from 'react-intl';
 import { Table, Icon, Tooltip } from 'antd';
 import moment from 'moment';
 import { v4 } from 'uuid';
+import _ from 'lodash';
 
 // proj
 import { MODALS } from 'core/modals/duck';
@@ -16,7 +17,12 @@ import Styles from './styles.m.css';
 class TasksTable extends Component {
     constructor(props) {
         super(props);
-        const { setModal, initOrderTasksForm, changeModalStatus, user } = this.props;
+        const {
+            setModal,
+            initOrderTasksForm,
+            changeModalStatus,
+            user,
+        } = this.props;
         const viewAllTasks = !isForbidden(user, permissions.GET_ALL_TASKS);
 
         this.columns = [
@@ -26,7 +32,10 @@ class TasksTable extends Component {
                 key:       'review',
                 width:     '4%',
                 render:    (text, record) => {
-                    if (record.orderNum && (viewAllTasks || user.id === record.responsibleId)) {
+                    if (
+                        record.orderNum &&
+                        (viewAllTasks || user.id === record.responsibleId)
+                    ) {
                         return (
                             <Icon
                                 className={ Styles.editOrderTaskIcon }
@@ -158,7 +167,7 @@ class TasksTable extends Component {
                     </div>
                 ),
             },
-           
+
             {
                 title:     <FormattedMessage id='author' />,
                 dataIndex: 'author',
@@ -190,11 +199,9 @@ class TasksTable extends Component {
 
         return (
             <Catcher>
-                <div                     className={ Styles.table }
-                >
+                <div className={ Styles.table }>
                     <Table
                         className={ Styles.ExpandedTable }
-
                         dataSource={
                             orderTasks.orderTasks
                                 ? [
@@ -212,22 +219,24 @@ class TasksTable extends Component {
                         scroll={ { x: 2000, y: 200 } }
                         columns={ columns }
                         pagination={ false }
-                        expandedRowClassName={ ()=>Styles.ExpandedRow }
-                        expandedRowRender={ ()=>{
-                            return <Table
-                                columns={ columns }
-                                dataSource={ [
-                                    ...orderTasks.orderTasks[ 0 ].history
-                                        .sort(this.sortHistory)
-                                        .map((task, index) => ({
-                                            ...task,
-                                            index,
-                                            key: v4(),
-                                        })),
-                                ] }
-                                pagination={ false }
-                                showHeader={ false }
-                            />
+                        expandedRowClassName={ () => Styles.ExpandedRow }
+                        expandedRowRender={ (record, index) => {
+                            return (
+                                <Table
+                                    columns={ columns }
+                                    dataSource={ [
+                                        ..._.get(orderTasks, [ 'orderTasks', index, 'history' ], [])
+                                            .sort(this.sortHistory)
+                                            .map((task, index) => ({
+                                                ...task,
+                                                index,
+                                                key: v4(),
+                                            })),
+                                    ] }
+                                    pagination={ false }
+                                    showHeader={ false }
+                                />
+                            );
                         } }
                         locale={ {
                             emptyText: <FormattedMessage id='no_data' />,
