2ba4b86c9a937d26e7026bbcddf835afecafdfbd
Bogdan Volotovskyy <bvol@carbook.ua>
2019-01-11 13:44:26 +0200

    improve(ui): cash creation form

--- source/forms/CashCreationForm/index.js
+++ source/forms/CashCreationForm/index.js
@@ -1,8 +1,8 @@
 // vendor
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Form, Select, Button } from 'antd';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { Form, Select, Icon } from 'antd';
+import { injectIntl } from 'react-intl';
 
 // proj
 import { createCashbox } from 'core/cash/duck';
@@ -10,6 +10,7 @@ import { DecoratedInput, DecoratedSelect } from 'forms/DecoratedFields';
 
 // own
 import { cashboxTypes } from './config';
+import Styles from './styles.m.css';
 const Option = Select.Option;
 
 const formItemLayout = {
@@ -24,8 +25,7 @@ const formItemLayout = {
     { createCashbox },
 )
 export class CashCreationForm extends Component {
-    _submit = e => {
-        e.preventDefault();
+    _submit = () => {
         const { form, createCashbox } = this.props;
         form.validateFields((err, values) => {
             if (!err) {
@@ -37,59 +37,61 @@ export class CashCreationForm extends Component {
 
     render() {
         const { getFieldDecorator } = this.props.form;
+        const { formatMessage } = this.props.intl;
 
         return (
-            <Form layout='horizontal' onSubmit={ this._submit }>
+            <Form
+                layout='horizontal'
+                className={ Styles.form }
+                onSubmit={ this._submit }
+            >
                 <DecoratedInput
                     field='name'
-                    formItem
-                    formItemLayout={ formItemLayout }
                     rules={ [
                         {
                             required: true,
-                            message:  this.props.intl.formatMessage({
+                            message:  formatMessage({
                                 id: 'cash-creation-form.name.validation',
                             }),
                         },
                     ] }
-                    hasFeedback
-                    label={ <FormattedMessage id='cash-creation-form.name' /> }
+                    placeholder={ formatMessage({
+                        id: 'cash-creation-form.name',
+                    }) }
                     getFieldDecorator={ getFieldDecorator }
+                    cnStyles={ Styles.field }
                 />
                 <DecoratedSelect
                     field='type'
-                    formItem
-                    formItemLayout={ formItemLayout }
-                    label={ <FormattedMessage id='cash-creation-form.type' /> }
+                    placeholder={ formatMessage({
+                        id: 'cash-creation-form.type',
+                    }) }
                     getFieldDecorator={ getFieldDecorator }
                     initialValue={ cashboxTypes.CASH }
+                    cnStyles={ Styles.field }
                 >
                     { Object.values(cashboxTypes).map(item => (
                         <Option value={ item } key={ item }>
-                            <FormattedMessage
-                                id={ `cash-creation-form.type-${item}` }
-                            />
+                            { formatMessage({
+                                id: `cash-creation-form.type-${item}`,
+                            }) }
                         </Option>
                     )) }
                 </DecoratedSelect>
                 <DecoratedInput
+                    fields={ {} }
+                    cnStyles={ Styles.field }
                     field='description'
-                    formItem
-                    formItemLayout={ formItemLayout }
-                    label={
-                        <FormattedMessage id='cash-creation-form.description' />
-                    }
+                    placeholder={ formatMessage({
+                        id: 'cash-creation-form.description',
+                    }) }
                     getFieldDecorator={ getFieldDecorator }
                 />
-                <Form.Item>
-                    <Button
-                        type='primary'
-                        htmlType='submit'
-                        style={ { margin: '0 auto' } }
-                    >
-                        <FormattedMessage id='create' />
-                    </Button>
-                </Form.Item>
+                <Icon
+                    type='save'
+                    onClick={ () => this._submit() }
+                    className={ Styles.saveIcon }
+                />
             </Form>
         );
     }
