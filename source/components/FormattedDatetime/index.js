c3fad9cafa97da0277848707f2baf96059213ff4
Bogdan Volotovskyy <bvol@carbook.ua>
2019-01-14 13:34:53 +0200

    improve(ui): formatted datetime format & cash order datetime

--- source/components/FormattedDatetime/index.js
+++ source/components/FormattedDatetime/index.js
@@ -4,10 +4,12 @@ import React from 'react';
 // own
 import moment from 'moment';
 
-const FormattedDatetime = ({ datetime }) =>(<div>
-    { datetime
-        ? moment(datetime).format('DD.MM.YYYY HH:mm')
-        : '-' }
-</div>);
+const FormattedDatetime = ({ datetime, format }) => (
+    <div>
+        { datetime
+            ? moment(datetime).format(format ? format : 'DD.MM.YYYY HH:mm')
+            : '-' }
+    </div>
+);
 
 export default FormattedDatetime;
