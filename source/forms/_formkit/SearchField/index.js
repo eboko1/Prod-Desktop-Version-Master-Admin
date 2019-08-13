2a7be515ce1c2948e2719f063e6f82a439886f92
Bohdan Volotovskyi <bohdan@timezynk.com>
2019-08-13 21:56:10 +0300

    improve(ui): added permission on storage search

--- source/forms/_formkit/SearchField/index.js
+++ source/forms/_formkit/SearchField/index.js
@@ -1,13 +1,14 @@
 // vendor
 import React from 'react';
+import { connect } from 'react-redux';
 import { Input } from 'antd';
 import { injectIntl } from 'react-intl';
 import _ from 'lodash';
 import styled from 'styled-components';
 
 // proj
-
 import { Catcher } from 'commons';
+import { permissions, isForbidden } from 'utils';
 
 // own
 const Search = Input.Search;
@@ -26,6 +27,10 @@ const SearchField = props => {
         <Catcher>
             <SearchWrapper width={ props.width }>
                 <Search
+                    disabled={ isForbidden(
+                        props.user,
+                        permissions.SEARCH_STORE_PRODUCT,
+                    ) }
                     placeholder={ props.intl.formatMessage({
                         id: 'storage.search',
                     }) }
@@ -36,4 +41,8 @@ const SearchField = props => {
     );
 };
 
-export default injectIntl(SearchField);
+const mapStateToProps = state => ({
+    user: state.auth,
+});
+
+export default injectIntl(connect(mapStateToProps)(SearchField));
