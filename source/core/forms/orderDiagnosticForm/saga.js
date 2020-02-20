4cc5e94a0caf01be82fe7d8047adacd405c4c481
marianfedkovich <marianfedkovich@gmail.com>
2020-02-20 18:17:11 +0200

    fix https

--- source/core/forms/orderDiagnosticForm/saga.js
+++ source/core/forms/orderDiagnosticForm/saga.js
@@ -1,7 +1,6 @@
 export async function sendDiagnosticAnswer(orderId, templateId, diagnosticId, processId, answer, comment, photo) {
     let token = localStorage.getItem('_my.carbook.pro_token');
-    console.log(token);
-    let url = `http://localhost:14281`; //"http://" + window.location.hostname;
+    let url = `https://localhost:14281`; //"http://" + window.location.hostname;
     let params = `/orders/diagnostics/answer?orderId=${orderId}&templateId=${templateId}&diagnosticId=${diagnosticId}&processId=${processId}&answer=${answer}`
 
     if(comment) params += `&comment=${comment}`;
@@ -12,8 +11,6 @@ export async function sendDiagnosticAnswer(orderId, templateId, diagnosticId, pr
         const response = await fetch(url, {
             method: 'PUT',
             headers: {
-                'Accept': 'application/text',
-                'Content-Type': 'application/text',
                 'Authorization': token,
             }
         });
