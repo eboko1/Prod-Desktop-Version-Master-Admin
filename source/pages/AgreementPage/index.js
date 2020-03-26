734652ec4bde03c1c1c5edcf1cc5ce192f955428
marianfedkovich <marianfedkovich@gmail.com>
2020-03-26 20:42:01 +0200

    services table fix

--- source/pages/AgreementPage/index.js
+++ source/pages/AgreementPage/index.js
@@ -76,7 +76,7 @@ class AgreementPage extends Component {
     updateData(data) {
         this.state.servicesList = data.labors.map((elem)=>{
             elem.checked = true;
-            elem.comment = "dsd";
+            elem.comment = "**description**";
             return elem;
         });
         this.state.detailsList = data.details.map((elem)=>{
