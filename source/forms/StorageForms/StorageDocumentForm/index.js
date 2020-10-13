2359cbbdb2de2a82620c9b1eba41f4cd405504ac
marianfedkovich <marianfedkovich@gmail.com>
2020-10-13 07:02:28 +0300

    storage fix 13.10

--- source/forms/StorageForms/StorageDocumentForm/index.js
+++ source/forms/StorageForms/StorageDocumentForm/index.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 import { injectIntl, FormattedMessage } from 'react-intl';
-import { Form, Button, Input, Table, Select, Icon, DatePicker, AutoComplete, InputNumber, Modal, TreeSelect, notification, Checkbox } from 'antd';
+import { Form, Button, Input, Table, Select, Icon, DatePicker, AutoComplete, InputNumber, Modal, TreeSelect, notification, Checkbox, Badge } from 'antd';
 import _ from 'lodash';
 import moment from 'moment';
 
@@ -24,6 +24,7 @@ const formItemStyle = {
         marginBottom: 0,
     }
 };
+const disabledSelectText = {color: 'var(--text)'};
 const mask = "0,0.00";
 const INCOME = 'INCOME',
       EXPENSE = 'EXPENSE',
@@ -53,16 +54,18 @@ class StorageDocumentForm extends Component {
             counterpartOptionInfo: {
                 value: undefined,
                 children: "",
-            }
+            },
+            warning: false,
         };
         this.hideModal = this.hideModal.bind(this);
         this.showModal = this.showModal.bind(this);
         this.editProduct = this.editProduct.bind(this);
     }
 
-    editProduct(key) {
+    editProduct(key, warning=false) {
         this.setState({
             editKey: key,
+            warning: warning,
             modalVisible: true,
         })
     }
@@ -76,6 +79,7 @@ class StorageDocumentForm extends Component {
     hideModal() {
         this.setState({
             modalVisible: false,
+            warning: false,
             editKey: undefined,
         })
     }
@@ -95,7 +99,7 @@ class StorageDocumentForm extends Component {
     }
  
     render() {
-        const { editKey, modalVisible, clientSearchValue, counterpartOptionInfo } = this.state;
+        const { editKey, modalVisible, clientSearchValue, counterpartOptionInfo, warning } = this.state;
         const {
             id,
             addDocProduct,
@@ -106,7 +110,8 @@ class StorageDocumentForm extends Component {
             brands,
             deleteDocProduct,
             editDocProduct,
-            clientList
+            clientList,
+            loading,
         } = this.props;
 
         const {
@@ -147,6 +152,7 @@ class StorageDocumentForm extends Component {
                         <Select
                             disabled={disabled || status==NEW}
                             value={type}
+                            style={disabledSelectText}
                             onChange={(value)=>{
                                 if(value == INCOME || value == ORDER) {
                                     updateFormData({
@@ -207,6 +213,7 @@ class StorageDocumentForm extends Component {
                         <Select
                             disabled={disabled || status==NEW}
                             value={documentType}
+                            style={disabledSelectText}
                             onChange={(value)=>{
                                 updateFormData({
                                     documentType: value,
@@ -236,6 +243,7 @@ class StorageDocumentForm extends Component {
                             showSearch
                             disabled={disabled || status==NEW}
                             value={counterpartId}
+                            style={disabledSelectText}
                             onChange={(value, option)=>{
                                 updateFormData({
                                     counterpartId: value,
@@ -305,6 +313,7 @@ class StorageDocumentForm extends Component {
                         <Select
                             disabled={type == INCOME || type == ORDER || disabled}
                             value={expenseWarehouseId}
+                            style={disabledSelectText}
                             onSelect={(value)=>{
                                 updateFormData({
                                     expenseWarehouseId: value,
@@ -328,6 +337,7 @@ class StorageDocumentForm extends Component {
                         <Select
                             disabled={type == EXPENSE || type == ORDER || type == RESERVE || disabled}
                             value={incomeWarehouseId}
+                            style={disabledSelectText}
                             onSelect={(value)=>{
                                 updateFormData({
                                     incomeWarehouseId: value,
@@ -352,6 +362,7 @@ class StorageDocumentForm extends Component {
                         <Input
                             disabled={disabled}
                             value={supplierDocNumber}
+                            style={{color: 'var(--text3)'}}
                             onChange={(event)=>{
                                 updateFormData({
                                     supplierDocNumber: event.target.value,
@@ -472,7 +483,7 @@ class StorageDocumentForm extends Component {
                         <FormattedMessage id="storage_document.pay_until" />
                         <DatePicker
                             style={{
-                                width: '100%'
+                                width: '100%',
                             }}
                             defaultValue={payUntilDatetime}
                             disabled={disabled || onlySum}
@@ -490,6 +501,7 @@ class StorageDocumentForm extends Component {
                 margin: "24px",
             }}>
                 <DocProductsTable
+                    loading={loading}
                     docProducts={docProducts}
                     disabled={disabled || !(status)}
                     updateFormData={updateFormData}
@@ -507,11 +519,13 @@ class StorageDocumentForm extends Component {
                         brands={brands}
                         addDocProduct={addDocProduct}
                         product={editKey !== undefined ? docProducts[editKey] : undefined}
+                        editKey={editKey}
                         editDocProduct={editDocProduct}
                         isIncome={type == INCOME}
                         priceDisabled={type == TRANSFER || type == RESERVE || documentType == OWN_CONSUMPTION}
                         warehouses={warehouses}
                         warehouseId={incomeWarehouseId || expenseWarehouseId}
+                        warning={warning}
                     /> 
                 : null}
             </div>
@@ -537,15 +551,7 @@ class DocProductsTable extends React.Component {
                 key:       'edit',
                 render:     (elem)=>{
                     return this.props.disabled ? null :
-                        elem.productId ?
-                            <Button
-                                disabled={this.props.disabled}
-                                onClick={()=>{
-                                    this.props.editProduct(elem.key);
-                                }}
-                            >
-                                <Icon type='edit' />
-                            </Button> : 
+                        !elem.detailCode ?
                             <Button
                                 type='primary'
                                 onClick={()=>{
@@ -553,7 +559,29 @@ class DocProductsTable extends React.Component {
                                 }}
                             >
                                 <Icon type='plus'/>
-                            </Button>
+                            </Button> :
+                            elem.productId ? 
+                                <Button
+                                    disabled={this.props.disabled}
+                                    onClick={()=>{
+                                        this.props.editProduct(elem.key);
+                                    }}
+                                >
+                                    <Icon type='edit' />
+                                </Button> :
+                                <Button
+                                    disabled={this.props.disabled}
+                                    type='primary'
+                                    style={{
+                                        backgroundColor: 'var(--approve)'
+                                    }}
+                                    onClick={()=>{
+                                        this.props.editProduct(elem.key, true);
+                                    }}
+                                >
+                                    <Icon type='warning'/>
+                                </Button>
+                            
                 }
             },
             {
@@ -679,10 +707,10 @@ class DocProductsTable extends React.Component {
     }
 
     render() {
-        const { disabled, docProducts } = this.props;
+        const { disabled, docProducts, loading } = this.props;
         var tableData = docProducts;
-        tableData = tableData.filter((elem)=>elem.productId)
-        if(!disabled && (!tableData.length || tableData[tableData.length-1].productId)) {
+        tableData = tableData.filter((elem)=>elem.detailCode);
+        if(!disabled && (!tableData.length || tableData[tableData.length-1].detailCode)) {
             tableData.push({
                 key: tableData.length,
                 productId: undefined,
@@ -693,6 +721,7 @@ class DocProductsTable extends React.Component {
                 columns={this.columns}
                 dataSource={tableData}
                 pagination={false}
+                loading={loading}
             />
         );
     }
@@ -978,20 +1007,37 @@ class AddProductModal extends React.Component {
             tradeCode
         } = productData;
 
-        this.props.addDocProduct({
-            productId: productId,
-            detailCode: detailCode,
-            brandName: brandName,
-            brandId: brandId,
-            tradeCode: tradeCode,
-            detailName: detailName,
-            stockPrice: stockPrice,
-            quantity: quantity,
-            sum: quantity*stockPrice,
-        });
-        this.setState({
-            alertModalVisible: false,
-        });
+        if(!this.props.warning) {
+            this.props.addDocProduct({
+                productId: productId,
+                detailCode: detailCode,
+                brandName: brandName,
+                brandId: brandId,
+                tradeCode: tradeCode,
+                detailName: detailName,
+                stockPrice: stockPrice,
+                quantity: quantity,
+                sum: quantity*stockPrice,
+            });
+            this.setState({
+                alertModalVisible: false,
+            });
+        } else {
+            this.props.editDocProduct(
+                this.props.product.key,
+                {
+                    productId: productId,
+                    detailCode: detailCode,
+                    brandName: brandName,
+                    brandId: brandId,
+                    tradeCode: tradeCode,
+                    detailName: detailName,
+                    stockPrice: stockPrice,
+                    quantity: quantity,
+                    sum: quantity*stockPrice,
+                }
+            );
+        }
         this.handleCancel();
         this.getStorageProducts();
     }
@@ -1030,6 +1076,7 @@ class AddProductModal extends React.Component {
             })
         }
         else {
+            console.log(this)
             if(editMode) {
                 this.props.editDocProduct(
                     this.props.product.key,
