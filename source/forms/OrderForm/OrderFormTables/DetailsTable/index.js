7cd502d8335c1332eeee10e9eed64caa747829dd
marianfedkovich <marianfedkovich@gmail.com>
2020-12-07 18:43:51 +0200

    repair map indicator fix

--- source/forms/OrderForm/OrderFormTables/DetailsTable/index.js
+++ source/forms/OrderForm/OrderFormTables/DetailsTable/index.js
@@ -1197,10 +1197,12 @@ export class ReserveButton extends React.Component {
                 updateDetail(detail.key, detail);
             }
             else {
-                const availableCount = response.notAvailableProducts[0].available;
+                const availableCount = response.notAvailableProducts[0].available,
+                      reservedCount = response.notAvailableProducts[0].reservedCount;
+                console.log(response);
                 confirm({
                     title: `${formatMessage({id: 'storage_document.error.available'})}. ${formatMessage({id: 'storage_document.warning.continue'})}`,
-                    content: `${formatMessage({id: 'storage_document.notification.available_from_warehouse'}, {name: detail.reservedFromWarehouseName})}: ${availableCount} ${formatMessage({id: 'pc'})}`,
+                    content: `${formatMessage({id: 'storage_document.notification.available_from_warehouse'}, {name: detail.reservedFromWarehouseName})}: ${availableCount} / ${availableCount - reservedCount} ${formatMessage({id: 'pc'})}`,
                     okButtonProps: {disabled: !availableCount},
                     onOk() {
                         data.docProducts[0].quantity = availableCount;
