3767135055637bde83fa2fc1c2ac62c3524c70f8
Marian Fedkovich <marianfedkovich@gmail.com>
2021-03-04 16:49:35 +0200

    order crash fix

--- source/pages/LaborsPage/index.js
+++ source/pages/LaborsPage/index.js
@@ -539,7 +539,7 @@ export default class LaborsPage extends Component {
             if(elem.deleted) {
                 deletedLabors.push(elem.laborId);
             }
-            else if(elem.changed && !elem.new) {
+            else if(elem.changed && !elem.new && elem.laborBusinessId) {
                 labors.push({
                     masterLaborId: elem.masterLaborId,
                     productId: elem.productId,
@@ -557,7 +557,7 @@ export default class LaborsPage extends Component {
                     labors[labors.length-1].normHours = elem.normHours ? elem.normHours : 1;
                 }
             }
-            else if(elem.new && elem.masterLaborId && elem.productId) {
+            else if((elem.new && elem.masterLaborId && elem.productId) || !elem.laborBusinessId && elem.laborId) {
                 newLabors.push({
                     masterLaborId: elem.masterLaborId,
                     productId: elem.productId,
