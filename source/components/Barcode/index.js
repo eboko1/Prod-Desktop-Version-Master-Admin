6841d9a5d71a16c8227aef676b69c3794fbfb16c
Marian Fedkovich <marianfedkovich@gmail.com>
2021-03-23 17:01:40 +0200

    barcode 400 error

--- source/components/Barcode/index.js
+++ source/components/Barcode/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { FormattedMessage, injectIntl } from "react-intl";
 import JsBarcode  from "jsbarcode"; //https://github.com/lindell/JsBarcode/wiki/Options
-import { Button, Icon, Modal, message, Input } from "antd";
+import { Button, Icon, Modal, message, Input, notification } from "antd";
 import { permissions, isForbidden, fetchAPI } from "utils";
 import _ from "lodash";
 
@@ -11,6 +11,36 @@ import _ from "lodash";
 
 // own
 import Styles from './styles.m.css';
+
+function replaceLocation(str, reverse) {
+    const replacer = {
+        "q": "й", "w": "ц", "e": "у", "r": "к", "t": "е", "y": "н", "u": "г",
+        "i": "ш", "o": "щ", "p": "з", "[": "х", "]": "ъ", "a": "ф", "s": "ы",
+        "d": "в", "f": "а", "g": "п", "h": "р", "j": "о", "k": "л", "l": "д",
+        ";": "ж", "'": "э", "z": "я", "x": "ч", "c": "с", "v": "м", "b": "и",
+        "n": "т", "m": "ь", ",": "б", ".": "ю", "/": ".",
+    };
+
+    reverse && Object.keys(replacer).forEach(key => {
+        let v = replacer[key]
+        delete (replacer[key])
+        replacer[v] = key
+    })
+
+    for (let i = 0; i < str.length; i++) {
+        if (replacer[str[i].toLowerCase()] != undefined) {
+            let replace;
+            if (str[i] == str[i].toLowerCase()) {
+                replace = replacer[str[i].toLowerCase()];
+            } else if (str[i] == str[i].toUpperCase()) {
+                replace = replacer[str[i].toLowerCase()].toUpperCase();
+            }
+            str = str.replace(str[i], replace);
+        }
+    }
+
+    return str;
+}
 
 const mapStateToProps = state => ({
     user: state.auth,
@@ -125,17 +155,35 @@ export default class Barcode extends Component {
         const codeWithPrefix = `${fullPrefix}-${scanedCode}`
         
         if(referenceId && table && scanedCode) {
-            await fetchAPI('POST', 'barcodes', undefined, [{
-                referenceId: String(referenceId),
-                table,
-                customCode: scanedCode,
-            }])
-        }
-        
-        if(onConfirm) {
+            try {
+                await fetchAPI(
+                    'POST', 
+                    'barcodes', 
+                    undefined, 
+                    [{
+                        referenceId: String(referenceId),
+                        table,
+                        customCode: scanedCode,
+                    }],
+                    {handleErrorInternally: true}
+                );
+                this.setState({
+                    inputCode : "",
+                })
+                notification.success({
+                    message: `Штрих-код задан`,
+                });
+                if(onConfirm) {
+                    onConfirm(scanedCode, fullPrefix, codeWithPrefix);
+                }
+            } catch(e) {
+                notification.error({
+                    message: `Штрих-код уже задан`,
+                });
+            }
+        } else if(onConfirm) {
             onConfirm(scanedCode, fullPrefix, codeWithPrefix);
         }
-
         this.handleCancel();
     }
 
@@ -259,7 +307,7 @@ export default class Barcode extends Component {
                             value={scanedInputValue}
                             onChange={({target})=>{
                                 this.setState({
-                                    scanedInputValue: target.value,
+                                    scanedInputValue: replaceLocation(target.value, true),
                                 })
                             }}
                             onPressEnter={()=>{
