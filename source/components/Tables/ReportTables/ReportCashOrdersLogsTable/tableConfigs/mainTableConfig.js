1347cb2024fa214e3e6f84a18cc4e2bd9b1b2341
GT <FTowerT@outlook.com>
2021-03-31 14:38:00 +0300

    My changes for cashdesk logs tables

---
+++ source/components/Tables/ReportTables/ReportCashOrdersLogsTable/tableConfigs/mainTableConfig.js
@@ -0,0 +1,92 @@
+// vendor
+import React from 'react';
+import { Input } from 'antd';
+import { FormattedMessage } from 'react-intl';
+import moment from 'moment';
+
+// proj
+import { Numeral } from 'commons';
+
+// own
+import Styles from './../styles.m.css';
+
+//Choose width for each col
+//It must be 100% of width in total!
+const defWidth = {
+    no: '4%',
+    client_name: 'auto',
+    order_num: '10%',
+    status: '10%',
+
+    date_created: '6%',
+    date_appointment: '6%',
+    date_done: '6%',
+
+    sum_labors: '5%',
+    sum_parts: '5%',
+    sum_total: '5%',
+
+    profit_labors: '5%',
+    profit_parts: '5%',
+    profit_total: '5%',
+
+    margin_labors: '5%',
+    margin_parts: '5%',
+    margin_total: '5%'
+}
+
+/* eslint-disable complexity */
+export default function columnsConfig(props) {
+
+    let count = 0;
+
+
+    const noCol = {
+        title:     <FormattedMessage id='report-orders-table.no' />,
+        align: 'left',
+        key: 'no',
+        width: defWidth.no,
+        render: () => count++
+        // render: (empty1, empty2, index) => ( <h4>{index+1+((filter.page-1)*25)}</h4>)
+    };
+
+    const col1 = {
+        title:     'ID',
+        dataIndex: 'id',
+    };
+
+    const col2 = {
+        title:     'Cash order number',
+        dataIndex: 'cashOrderNumber',
+    };
+
+    const col22 = {
+        title: 'Order number',
+        dataIndex: 'orderNumber'
+    }
+
+    const col3 = {
+        title:     'Data',
+        dataIndex: 'data',
+    };
+
+    const col4 = {
+        title:     'Sum',
+        dataIndex: 'sum',
+    };
+
+    const col5 = {
+        title:     'Fiscal',
+        dataIndex: 'fiscal',
+    };
+   
+    return [
+        noCol,
+        col1,
+        col2,
+        col22,
+        col3,
+        col4,
+        col5
+    ];
+}
---
+++ source/core/reports/reportCashOrdersLogs/saga.js
@@ -0,0 +1,37 @@
+// vendor
+import { call, put, all, take, select } from 'redux-saga/effects';
+
+//proj
+import { fetchAPI } from 'utils';
+import { emitError } from 'core/ui/duck';
+
+// own
+import {
+    fetchCashOrdersLogsSuccess,
+} from './duck';
+
+import {
+    FETCH_CASH_ORDER_LOGS
+} from './duck';
+
+export function* fetchCashOrdersLogsSaga() {
+    while (true) {
+        try {
+            yield take(FETCH_CASH_ORDER_LOGS);
+
+            const data = yield call(
+                fetchAPI,
+                'GET',
+                `/cashdesk/logs`,
+                {page: 1}
+            );
+            yield put(fetchCashOrdersLogsSuccess(data));
+        } catch(err) {
+            emitError(err);
+        }
+    }
+}
+
+export function* saga() {
+    yield all([ call(fetchCashOrdersLogsSaga) ]);
+}
