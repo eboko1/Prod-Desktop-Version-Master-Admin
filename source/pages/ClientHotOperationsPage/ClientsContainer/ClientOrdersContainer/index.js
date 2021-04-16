e4d0c1251936895e1dd298be19c37dee5b0e473a
GT <FTowerT@outlook.com>
2021-04-16 18:38:57 +0300

    Pagination bug fixed: default pagination is working properly anyway

--- source/pages/ClientHotOperationsPage/ClientsContainer/ClientOrdersContainer/index.js
+++ source/pages/ClientHotOperationsPage/ClientsContainer/ClientOrdersContainer/index.js
@@ -4,8 +4,8 @@ Container used to show clients and perform basic search of them.
 //Vendor
 import React from 'react';
 import { connect } from "react-redux";
-import { FormattedMessage, injectIntl } from 'react-intl';
-import { Table, Input } from 'antd';
+import { injectIntl } from 'react-intl';
+import { Table } from 'antd';
 import { v4 } from 'uuid';
 
 //Own
@@ -15,7 +15,6 @@ import Styles from './styles.m.css';
 const mapStateToProps = state => ({
     user: state.auth,
     orders: state.clientHotOperations.clientOrdersData.orders,
-    stats: state.clientHotOperations.clientOrdersData.stats,
     clientOrdersFetching: state.clientHotOperations.clientOrdersFetching
 });
 
@@ -33,17 +32,9 @@ export default class ClientOrdersContainer extends React.Component {
     render() {
         const {
             orders,
-            stats,
             clientOrdersFetching,
             intl: {formatMessage}
         } = this.props;
-
-        const pagination = {
-            pageSize: 25,
-            size: "large",
-            total: Math.ceil(stats.countOrders / 25) * 25,
-            hideOnSinglePage: true,
-        };
 
         return (
             <div className={Styles.tableCont}>
@@ -54,7 +45,6 @@ export default class ClientOrdersContainer extends React.Component {
                     columns={columnsConfig({formatMessage})}
                     scroll={ { x: 1000, y: '30vh' } }
                     loading={clientOrdersFetching}
-                    pagination={pagination}
                     rowKey={() => v4()}
                     bordered
                 />
