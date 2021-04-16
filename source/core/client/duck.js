1e92ade212400315c68ac908a7a09c8bbb311e02
GT <FTowerT@outlook.com>
2021-04-16 19:27:06 +0300

    Ability to create a new order from client's page is added

--- source/core/client/duck.js
+++ source/core/client/duck.js
@@ -7,6 +7,8 @@ const prefix = `cpb/${moduleName}`;
 export const FETCH_CLIENT = `${prefix}/FETCH_CLIENT`;
 export const FETCH_CLIENT_SUCCESS = `${prefix}/FETCH_CLIENT_SUCCESS`;
 
+export const CREATE_ORDER_FOR_CLIENT = `${prefix}/CREATE_ORDER_FOR_CLIENT`;
+
 export const CREATE_CLIENT_VEHICLE = `${prefix}/CREATE_CLIENT_VEHICLE`;
 export const UPDATE_CLIENT_VEHICLE = `${prefix}/UPDATE_CLIENT_VEHICLE`;
 export const DELETE_CLIENT_VEHICLE = `${prefix}/DELETE_CLIENT_VEHICLE`;
@@ -61,4 +63,17 @@ export const fetchClient = id => ({
 export const fetchClientSuccess = clientEntity => ({
     type:    FETCH_CLIENT_SUCCESS,
     payload: clientEntity,
+});
+
+/**
+ * Create new order(н/з)  which will contain specific client.
+ * New client will be fetched by id , is is used to pass differences in data from different routes
+ * @param {*} param0 {
+ *      clientId - id of a client,
+ *      managerId - id of a manager who created an order(current user)
+ *      }
+ */
+ export const createOrderForClient = ({clientId, managerId}) => ({
+    type: CREATE_ORDER_FOR_CLIENT,
+    payload: {clientId, managerId}
 });
