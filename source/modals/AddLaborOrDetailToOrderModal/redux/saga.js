9aee4f866261921700eb44312d9d2579816d7957
GT <FTowerT@outlook.com>
2021-06-02 21:09:01 +0300

    Added error validation for errors in sagas of AddLaborOrDetailToOrderModal

--- source/modals/AddLaborOrDetailToOrderModal/redux/saga.js
+++ source/modals/AddLaborOrDetailToOrderModal/redux/saga.js
@@ -1,5 +1,6 @@
 // vendor
 import { call, put, all, take, select } from 'redux-saga/effects';
+import { emitError } from 'core/ui/duck';
 import _ from 'lodash';
 import history from 'store/history';
 import book from 'routes/book';
@@ -25,17 +26,17 @@ import {
 
 export function* fetchOrdersSaga() {
     while (true) {
-        yield take(FETCH_ORDERS);
-
-        yield put(setOrdersFetching(true));
-
-        const query = yield select(selectOrdersQuery);
-
         try{
+            yield take(FETCH_ORDERS);
+            yield put(setOrdersFetching(true));
+
+            const query = yield select(selectOrdersQuery);
+
             const { orders, stats } = yield call(fetchAPI, 'GET', `orders`, { ...query }, null, {handleErrorInternally: true});
+
             yield put(fetchOrdersSuccess({ orders, stats }));
-        } catch(err) {
-            console.log(err, query);
+        } catch(error) {
+            yield put(emitError(error));
         } finally {
             yield put(setOrdersFetching(false));
         }        
@@ -44,80 +45,74 @@ export function* fetchOrdersSaga() {
 
 export function* addLaborsToOrderSaga() {
     while (true) {
-        yield take(ADD_LABORS_TO_ORDER);
-
-        const selectedOrderId = yield select(selectSelectedOrderId);
-        const labors = yield select(selectLabors);
-
-        console.log("Data: ", labors, selectedOrderId);
-
-        if(!labors || !selectedOrderId) continue;
-
-        const payload = {
-			insertMode: true,
-			details: [],
-			services: [],
-		};
-
-        _.each(labors, labor => {
-            payload.services.push({
-                serviceId: labor.laborId,
-                serviceName: labor.serviceName || labor.defaultName,
-                employeeId: labor.employeeId,
-                serviceHours: 0,
-                purchasePrice: 0,
-                count: Number(labor.hours) || 0,
-                servicePrice: Number(labor.price),
-            });
-        });        
-
         try{
+            yield take(ADD_LABORS_TO_ORDER);
+
+            const selectedOrderId = yield select(selectSelectedOrderId);
+            const labors = yield select(selectLabors);
+
+            const payload = {
+                insertMode: true,
+                details: [],
+                services: [],
+            };
+
+            _.each(labors, labor => {
+                payload.services.push({
+                    serviceId: labor.laborId,
+                    serviceName: labor.serviceName || labor.defaultName,
+                    employeeId: labor.employeeId,
+                    serviceHours: 0,
+                    purchasePrice: 0,
+                    count: Number(labor.hours) || 0,
+                    servicePrice: Number(labor.price),
+                });
+            });
+        
             yield call(fetchAPI, 'PUT', `orders/${selectedOrderId}`, null, payload, {handleErrorInternally: true});
+
             history.push({
                 pathname: `${book.order}/${selectedOrderId}`,
             });
-        } catch(err) {
-            console.log(err, query);
+        } catch(error) {
+            yield put(emitError(error));
         }
     }
 }
 
 export function* addDetailsToOrderSaga() {
     while (true) {
-        yield take(ADD_DETAILS_TO_ORDER);
-
-        const selectedOrderId = yield select(selectSelectedOrderId);
-        const details = yield select(selectDetails);
-
-        console.log("Data: ", details, selectedOrderId);
-
-        if(!details || !selectedOrderId) continue;
-
-        const payload = {
-			insertMode: true,
-			details: [],
-			services: [],
-		};
-
-        _.each(details, product => {
-            payload.details.push({
-                storeGroupId: product.storeGroupId,
-                name: product.name,
-                productCode: product.productCode,
-                supplierBrandId: product.supplierBrandId,
-                count: 1,
-                price: 0,
-                purchasePrice: 0,
-            })
-        });
-
         try{
+            yield take(ADD_DETAILS_TO_ORDER);
+
+            const selectedOrderId = yield select(selectSelectedOrderId);
+            const details = yield select(selectDetails);
+
+            const payload = {
+                insertMode: true,
+                details: [],
+                services: [],
+            };
+
+            _.each(details, product => {
+                payload.details.push({
+                    storeGroupId: product.storeGroupId,
+                    name: product.name,
+                    productCode: product.productCode,
+                    supplierBrandId: product.supplierBrandId,
+                    count: 1,
+                    price: 0,
+                    purchasePrice: 0,
+                })
+            });
+
+        
             yield call(fetchAPI, 'PUT', `orders/${selectedOrderId}`, null, payload, {handleErrorInternally: true});
             history.push({
                 pathname: `${book.order}/${selectedOrderId}`,
-            });
-        } catch(err) {
-            console.log(err, query);
+            });            
+        } catch(error) {
+            yield put(emitError(error));
         }
     }
 }
